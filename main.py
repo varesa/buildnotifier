a35b28f5eab19159bec25c5eb9d84b26c0d667d1
Esa Varemo <esa@kuivanto.fi>
2018-04-16 09:48:28 +0300

    Add an endpoint to receive build information

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/post', methods=['POST'])
+def post():
+    print(request.form)
+
 @app.route('/health')
 def healthcheck():
     return 'OK'
