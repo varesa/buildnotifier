bb58834fedf249b41b6f7efcc75c2c4991cf8342
Esa Varemo <esa@kuivanto.fi>
2018-04-16 10:15:15 +0300

    Add telegram initialization

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
-from time import sleep
 from flask import Flask, redirect, render_template, request
+from telegram.ext import Updater, CommandHandler
+from time import sleep
 
+logger = logging.getLogger("buildnotifier")
+logger.setLevel(LOG_LEVEL)
+handler = logging.StreamHandler()
+handler.setLevel(LOG_LEVEL)
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
 
-# Initialize app
+# Telegram
+
+def start(bot, update):
+    """
+    Handler for the /start command. Send instructions
+    """
+    logger.info("Received /start. Chat ID:" + str(update.message.chat_id))
+    logger.info("Sending instructions")
+
+    bot.send_message(chat_id=update.message.chat_id, text="Hello. Type /builds to receive build status notifications or /full to get full build information")
+
+full = []
+
+def full(bot, update):
+	logger.info("Received /full. Chat ID: " + str(update.message.chat_id))
+	full.append(update.message.chat_id)
+	bot.send_message(chat_id=update.message.chat_id, text="You will now receive full build information. Type /off to disable")
+
+logger.info("Starting")
+
+token = os.environ['TELEGRAM_TOKEN']
+
+# Initialize the telegram API
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+
+# Register the command handlers
+start_handler = CommandHandler('start', start)
+full_handler = CommandHandler('full', full)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(full_handler)
+
+logger.info("Starting telegram polling")
+updater.start_polling()
+
+# Flask
 
 app = Flask(__name__)
 
