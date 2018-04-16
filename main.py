efd41a179b6ae84f38cb9454a6626da8ff5dac17
Esa Varemo <esa@kuivanto.fi>
2018-04-16 17:19:29 +0300

    Send messages on build stages

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import logging
 from telegram.ext import Updater, CommandHandler
 from time import sleep
 
+
+###########
+# Logging #
+###########
+
 logger = logging.getLogger("buildnotifier")
 logger.setLevel(LOG_LEVEL)
 handler = logging.StreamHandler()
@@ -10,7 +15,10 @@ handler.setLevel(LOG_LEVEL)
 handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(handler)
 
-# Telegram
+
+############
+# Telegram #
+############
 
 def start(bot, update):
     """
@@ -23,7 +31,7 @@ def start(bot, update):
 
 full = []
 
-def full(bot, update):
+def register_full(bot, update):
 	logger.info("Received /full. Chat ID: " + str(update.message.chat_id))
 	full.append(update.message.chat_id)
 	bot.send_message(chat_id=update.message.chat_id, text="You will now receive full build information. Type /off to disable")
@@ -38,14 +46,22 @@ dispatcher = updater.dispatcher
 
 # Register the command handlers
 start_handler = CommandHandler('start', start)
-full_handler = CommandHandler('full', full)
+full_handler = CommandHandler('full', register_full)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(full_handler)
 
 logger.info("Starting telegram polling")
 updater.start_polling()
 
-# Flask
+def send_stage(stage):
+    for chat_id in full:
+        bot.send_message(chat_id=chat_id, text="Build entered stage " + stage)
+
+#########
+# Flask #
+#########
+
+logger.info("Starting flask")
 
 app = Flask(__name__)
 
@@ -53,13 +69,15 @@ app = Flask(__name__)
 def post():
     data = request.json
     if data['type'] == 'stage':
-        print('Running stage: ' + data['stage'])
+        logger.info('Data received: Running stage: ' + data['stage'])
+        send_stage(data['stage'])
 
     if data['type'] == 'complete':
-        print('Build complete: ' + data['url'])
+        logger.info('Data received: Build complete: ' + data['url'])
     return ''
 
 @app.route('/health')
 def healthcheck():
     return 'OK'
 
+logger.info("running")
